7ab01825b185ab57d762a2c9c492e9ea2860e2ca
abezio <69462711+abelpacifico@users.noreply.github.com>
2020-09-07 09:38:55 -0300

    Create hola.sh

---
+++ hola.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Content-type: text/html"
+echo ""
+
+echo '<html>'
+echo '<head>'
+echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'
+echo '<title>Hello World</title>'
+echo '</head>'
+echo '<body>'
+echo 'Hello World'
+echo '</body>'
+echo '</html>'
+
+exit 0
